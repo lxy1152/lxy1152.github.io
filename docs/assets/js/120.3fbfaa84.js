aad99edec4c451b59534726e08bb55ad56c8cd75
Xiangyu Li <2676092082@qq.com>
2021-08-16 19:51:05 +0800

    build: 生成静态文件
    生成静态文件

--- docs/assets/js/120.3fbfaa84.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[120],{670:function(n,w,o){}}]);
---
+++ docs/assets/js/19.8722309b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{673:function(n,w,o){}}]);
